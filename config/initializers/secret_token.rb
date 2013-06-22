7ecc78a5f74c6c89d91411a847a42f1c1b1cb013
John Howlett <jehowl4@gmail.com>
2013-06-22 17:16:57 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+VidApp::Application.config.secret_token = '4c38ee0c381cf6514ce771ec3c17c33a3793cc9e6e7d02bda990ef41cb5f890a60233a6ee129c6b005e83a9632a1c6867849cb140391068779042769843c9691'
