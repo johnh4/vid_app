f17834be66e9f55a579417b55dc4c34524ce179a
John Howlett <jehowl4@gmail.com>
2013-06-27 01:46:16 -0400

    Add next/prev buttons, country dropdown

--- app/assets/javascripts/youtube.js
+++ app/assets/javascripts/youtube.js
@@ -12,13 +12,22 @@ $(document).on('ajax:complete ready', function(){
     swfobject.embedSWF("http://www.youtube.com/apiplayer?enablejsapi=1&version=3&playerapiid=ytplayer",
                         "ytplayer_div", width, "500", "9", null, null, params, atts);
 
+    var vid_index = 0;
+    var id_arr;
 
     $('.yt_button').click(function(){
         if ($(this).attr('now_playing') == 'false'){
-            var song_id = $(this).attr('yt_id');
-            ytplayer.loadVideoById(song_id, 1, 'large');
+            //var song_id = $(this).attr('yt_id');
+            //ytplayer.loadVideoById(song_id, 1, 'large');
+            var news_ids = $(this).attr('yt_id');
+            id_arr = news_ids.split(" ");
+            console.log(news_ids);
+            console.log(id_arr);
+            news_id = id_arr[0];
+            console.log(news_id);
+            ytplayer.loadVideoById(news_id, 1, 'large');
             $('.yt_button').each(function(){
-                if (song_id == $(this).attr('yt_id')){
+                if (news_ids == $(this).attr('yt_id')){
                     $(this).attr('now_playing', 'true');
                 }else{
                     $(this).attr('now_playing', 'false');
@@ -33,5 +42,59 @@ $(document).on('ajax:complete ready', function(){
         };
     });
 
+    $('.next_button').click(function(){
+        if( (vid_index) < ($(this).attr('num_vids')-1) ) {
+            vid_index++;
+            news_id = id_arr[vid_index];
+            ytplayer.loadVideoById(news_id, 1, 'large');
+            console.log(news_id);
+        }
+        else if (vid_index == ($(this).attr('num_vids')-1) ) {
+            vid_index = 0;
+            news_id = id_arr[vid_index];
+            ytplayer.loadVideoById(news_id, 1, 'large');
+            console.log(news_id);
+        }
+    });
+
+    $('.prev_button').click(function(){
+        if( (vid_index) > 0 ) {
+            vid_index--;
+            news_id = id_arr[vid_index];
+            ytplayer.loadVideoById(news_id, 1, 'large');
+            console.log(news_id);
+        }
+        else if (vid_index == 0 ) {
+            vid_index = $(this).attr('num_vids') - 1;
+            news_id = id_arr[vid_index];
+            ytplayer.loadVideoById(news_id, 1, 'large');
+            console.log(news_id);
+        }
+    });
+
+    
+
 
 });
+function loadVideo() {
+        var select_box = document.getElementById("country_selection");
+        var vid_ids = select_box.options[select_box.selectedIndex].value;
+        console.log(vid_ids);
+        var vid_arr = vid_ids.split(" ");
+        vid_index = 0;
+
+        //id_arr = vid_ids.split(" ");
+        //console.log(vid_ids);
+        var opt_id = vid_arr[0];
+        console.log(opt_id);
+        ytplayer.loadVideoById(opt_id, 1, 'large');
+
+        //document.getElementsByClassName('yt_button').yt_id = ' @vid.get_youtube_id_array("'+country_name+' music lil wayne") ';
+        //console.log(document.getElementsByClassName('yt_button').yt_id);
+        //var news_ids = $('.yt_button').attr('yt_id');;
+        //id_arr = news_ids.split(" ");
+        //console.log(id_arr);
+        //news_id = id_arr[0];
+        //console.log(news_id);
+        //ytplayer.loadVideoById(news_id, 1, 'large');
+}
--- app/models/video.rb
+++ app/models/video.rb
@@ -6,9 +6,16 @@ def initialize
 end
 
 def get_youtube_id(sought)
-	vid = @youtube_client.videos_by(:query => sought, :categories => [:music])
+	vid = @youtube_client.videos_by(:query => sought, :format => 5, :safe_search => 'strict', :restriction => 'US', :categories => [:news], time: :today)
 	return_vid = vid.videos.first.video_id.split(':').last
 	return_vid
+end
+
+def get_youtube_id_array(sought)
+	vid = @youtube_client.videos_by(:query => sought,  :format => 5, :safe_search => 'strict', :restriction => 'US', :categories => [:news], :time => :today)
+	vid_ids = Array.new
+	vid.videos.each { |video| vid_ids.push(video.video_id.split(':').last) }
+	vid_ids
 end
 
 end
